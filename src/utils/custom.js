0865d9c71b21aab3302ab627221c0761da7a63ad
Noman Chowdhury <noman4033>
2024-08-30 22:19:24 +0600

    navbar is completed aroud 70%

---
+++ src/utils/custom.js
@@ -0,0 +1,8 @@
+export const navbar = [
+    {nam:'home',id:34,path:'/'},
+    {nam:'about',id:34324,path:'/about'},
+    {nam:'product',id:346456,path:'/product'},
+    {nam:'man',id:39784,path:'/man'},
+    {nam:'woman',id:34234,path:'/woman'}
+];
+
