f2a0aabf156b7a62dab32f7b77cd4bf4b216d4ea
noman4033 <nomanafi2017@gmail.com>
2024-09-21 15:17:09 +0600

    ProudctMan section animation & section work 1% done

--- src/component/manPage/Man.jsx
+++ src/component/manPage/Man.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroMan from './HeroMan'
 import AboutMan from './AboutMan'
+import ProductMan from './ProductMan'
 
 const Man = () => {
   return (
     <div>
         <HeroMan></HeroMan>
         <AboutMan></AboutMan>
+        <ProductMan></ProductMan>
     </div>
   )
 }
